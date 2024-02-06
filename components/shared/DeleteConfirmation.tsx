d711cadf59ab66af3153c310eec08158ebaaf60c
Hamza Aoun <aou4925@gmail.com>
2024-02-06 14:31:16 +0200

    implement stripe

--- components/shared/DeleteConfirmation.tsx
+++ components/shared/DeleteConfirmation.tsx
@@ -1,9 +1,7 @@
-import React from 'react'
+import React from "react";
 
 const DeleteConfirmation = () => {
-  return (
-    <div>DeleteConfirmation</div>
-  )
-}
+  return <div>DeleteConfirmation</div>;
+};
 
-export default DeleteConfirmation
+export default DeleteConfirmation;
